bff88a2f3052e4412bb7cbd1c10e041d5e64f3eb
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2022-06-14 09:12:32 -0300

    Minha solução

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface IContentProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+  title: string;
+}
+
+export function Content({ movies, title }: IContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface ISideBarProps {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: ISideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
